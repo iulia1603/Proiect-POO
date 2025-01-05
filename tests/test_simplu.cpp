c630691546b8d9d6def1ea2f112f75d01cbbacd5
iulia1603 <148717801+iulia1603@users.noreply.github.com>
2025-01-05 16:47:22 +0200

    Initialize tests/Boost-ext-ut

---
+++ tests/test_simplu.cpp
@@ -0,0 +1,27 @@
+#include <ut.hpp>
+
+int sum(const int a, const int b) {
+    return a + b;
+}
+
+int main() {
+    using namespace boost::ut;
+
+
+    "sum of positive numbers"_test = [] {
+        // cppcheck-suppress knownConditionTrueFalse
+        expect(sum(2, 3) == 5_i);
+    };
+
+    "sum with zero"_test = [] {
+        // cppcheck-suppress knownConditionTrueFalse
+        expect(sum(5, 0) == 5_i);
+    };
+
+    "sum of negative numbers"_test = [] {
+        // cppcheck-suppress knownConditionTrueFalse
+        expect(sum(-2, -3) == -5_i);
+    };
+
+    return 0;
+}
